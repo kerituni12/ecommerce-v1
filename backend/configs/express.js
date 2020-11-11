7210bc1671b01b823573fed86dd8172742124f65
Hieu Nguyen <kerituni123@gmail.com>
2020-11-11 17:38:37 +0700

    some changes

--- backend/configs/express.js
+++ backend/configs/express.js
@@ -25,10 +25,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use(
-  cors(
-    { credentials: true, origin: "http://localhost:3000" },
-    { credentials: true, origin: "https://ecommerce-v1.vercel.app/" }
-  )
+  cors({ credentials: true, origin: "http://localhost:3000" }, { credentials: true, origin: process.env.FRONTEND_URL })
 );
 
 //Router
