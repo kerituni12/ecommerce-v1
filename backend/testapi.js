29554214269e48b30546d64fc2be4234060699a4
Hieu Nguyen <kerituni123@gmail.com>
2020-11-11 15:53:44 +0700

    update deploy

--- backend/testapi.js
+++
@@ -1,8 +0,0 @@
-const axios = require("axios");
-axios.post("http://localhost:3000/v1/auth/login")
-  .then(function (response) {
-    console.log("vo day",response);
-  })
-  .catch(function (error) {
-    console.log( error.response.data);   
-  });
