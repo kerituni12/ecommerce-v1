3eefd9c8f8b240da7c76064fa9034ab83fbc8206
Hieu Nguyen <kerituni123@gmail.com>
2020-11-19 16:14:43 +0700

    cors *

--- backend/configs/express.js
+++ backend/configs/express.js
@@ -14,7 +14,12 @@ const { logs } = require("@configs/constants");
 
 const FRONTEND_BUILD_PATH = path.join(__dirname, "../../frontend/build");
 const regexDomain = new RegExp(`/\.kerituni12\.vercel\.app$/`, "g");
-const allowedDomains = [regexDomain, "https://localhost:3000", process.env.FRONTEND_URL, "https://kinshop.tk"];
+const allowedDomains = [
+  /\.kerituni12\.vercel\.app$/,
+  "https://localhost:3000",
+  process.env.FRONTEND_URL,
+  "https://kinshop.tk",
+];
 
 const app = express();
 
